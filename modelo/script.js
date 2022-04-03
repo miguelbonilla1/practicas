674bb83c9dca4276a64ac0302efe9868966afd78
Miguel Bonilla <miguelbonilla1886@gmail.com>
2022-04-03 17:14:46 -0400

    practicas

---
+++ modelo/script.js
@@ -0,0 +1,48 @@
+const mybutton = document.querySelector('.botton')
+const container = document.querySelector(".container")
+const buttonRespuesta = document.querySelector(".respuesta")
+var img = document.querySelector("#image")
+
+
+
+
+
+const firstInput = document.querySelector (".first-input")
+
+const secondInput = document.querySelector (".second-input")
+
+
+
+
+mybutton.addEventListener("click", (e) =>{
+    container.style.display = "block"
+
+
+})
+
+
+buttonRespuesta.addEventListener("click",() =>{
+    console.log(firstInput.value)
+
+    if (firstInput.value === "aguilar" && secondInput.value === "cestau") {
+        alert("Respuesta correcta!") 
+            img.style.display = "block"
+       }
+
+       else if(!firstInput.value || !secondInput.value) {
+        alert("escribe un texto wey")
+        container.style.display ="none"
+        }
+    
+        
+        else {
+    alert("Respuesta incorrecta!")
+       }
+})
+
+  
+
+
+
+
+
